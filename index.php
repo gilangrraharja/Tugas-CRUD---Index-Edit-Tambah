93d831b1b10ac90d4e8fd12cdab3688c407c8d95
R Gilang Rizkia Raharja <gilangrizkia331@gmail.com>
2017-02-04 20:26:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html>
+<HEAD>
+	<title>KULIAH WEB</title>
+</HEAD>
+<link rel="stylesheet" type="text/css" href="style.css">
+<body>
+<div class="container">
+	<header>
+		<h1>CRUD Operation</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><b>MENU</b></li>
+			<li><a href="#">Menu 1</a></li>
+			<li><a href="#">Menu 2</a></li>
+			<li><a href="#">Menu 3</a></li>
+			<li><a href="#">Menu 4</a></li>
+		</ul>
+	</nav>
+	<article>
+		<table border="1" width="75%" align="center">
+			<tr>
+				<th><a href="tambah.php"><button type="submit">+ TAMBAH</button></a></th>
+			</tr>
+			<tr>
+				<td>No</td>
+				<td>Nama</td>
+				<td>Nim</td>
+				<td>Alamat</td>
+				<td>Aksi</td>
+			</tr>
+		</table>
+	</article>
+	<footer>R Gilang Rizkia R - 14.111.076</footer>
+</div>
+</body>
+</html>
---
+++ tambah.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+<HEAD>
+	<title>KULIAH WEB</title>
+</HEAD>
+<link rel="stylesheet" type="text/css" href="style.css">
+<body>
+<div class="container">
+	<header>
+		<h1>CRUD Operation</h1>
+	</header>
+	<nav>
+		<ul>
+			<li><b>MENU</b></li>
+			<li><a href="#">Menu 1</a></li>
+			<li><a href="#">Menu 2</a></li>
+			<li><a href="#">Menu 3</a></li>
+			<li><a href="#">Menu 4</a></li>
+		</ul>
+	</nav>
+	<article>
+		<table border="1" width="75%" align="center">
+			<tr>
+				<th>Tambah Data</th>
+			</tr>
+			<tr>
+				<td>Nama</td>
+				<td>:</td>
+				<td><input type="text" name="nama" size="80"></input></td>
+			</tr>
+			<tr>
+				<td>NIM</td>
+				<td>:</td>
+				<td><input type="text" name="nim" size="80"></input></td>
+			</tr>
+			<tr>
+				<td>Alamat</td>
+				<td>:</td>
+				<td><input type="text" name="alamat" size="80"></input></td>
+			</tr>
+			<tr>
+				
+				<td colspan="3"><input type="button" value="SIMPAN"></input><input type="button" value="BATAL"></input></td>
+			</tr>
+		</table>
+	</article>
+	<footer>R Gilang Rizkia R - 14.111.076</footer>
+</div>
+</body>
+</html>
